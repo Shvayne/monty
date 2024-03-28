db7d4ed30bcd878a3f2bdf9482bcc41ceec922cc
Christiana Apinoko <christyapinoko@gmail.com>
2024-03-28 09:46:46 -0700

    fixed betty

--- main.c
+++ main.c
@@ -1,52 +1,52 @@
 #include "monty.h"
+
+line_t data = {0, NULL, NULL, NULL, NULL};
 /**
  * main - entry point for interpreter
  * @ac: argument count
  * @av: arg vector
  * Return: sucess or failure
 */
-line_t data = {0, NULL, NULL, NULL, NULL};
-
 int main(int ac, char **av)
 {
-    FILE *fp;
-    char *line = NULL;
-    size_t len = 0;
-    stack_t *stack = NULL;
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	stack_t *stack = NULL;
 
-    if(ac != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-    fp = fopen(av[1], "rb");
-    if (fp == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", av[1]);
-        exit(EXIT_FAILURE);
-    }
-    data.fp = fp;
-    while ((_getline(&line, &len, fp)) > 0)
-    {
-        data.line = line;
-        data.num += 1;
-        if (split_line() == 0)
-        {
-            free(line);
-            continue;
-        }
-        if (_execute(&stack) == -1)
-        {
-            fprintf(stderr, "L%d: unknown instruction %s\n", data.num, data.cmd);
-            free(line);
-            fclose(fp);
-            _free(&stack);
-            exit(EXIT_FAILURE);
-        }
-        free(line);
-    }
-    free(line);
-    _free(&stack);
-    fclose(fp);
-    return (EXIT_SUCCESS);
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	fp = fopen(av[1], "rb");
+	if (fp == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
+	data.fp = fp;
+	while ((_getline(&line, &len, fp)) > 0)
+	{
+		data.line = line;
+		data.num += 1;
+		if (split_line() == 0)
+		{
+			free(line);
+			continue;
+		}
+		if (_execute(&stack) == -1)
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", data.num, data.cmd);
+			free(line);
+			fclose(fp);
+			_free(&stack);
+			exit(EXIT_FAILURE);
+		}
+		free(line);
+	}
+	free(line);
+	_free(&stack);
+	fclose(fp);
+	return (EXIT_SUCCESS);
 }
--- push_pall_pint.c
+++ push_pall_pint.c
@@ -7,53 +7,51 @@
 */
 void push(stack_t **stack, unsigned int line_num)
 {
-    stack_t *new_node = malloc(sizeof(stack_t));
-    int i, n;
+	stack_t *new_node = malloc(sizeof(stack_t));
+	int i, n;
 
-    if (new_node == NULL)
-    {
-        free(data.line);
-        _free(stack);
-        fclose(data.fp);
-        fprintf(stderr, "Error: Malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-    if (data.arg == NULL)
-    {
-        free(new_node);
-        free(data.line);
-        _free(stack);
-        fclose(data.fp);
-        fprintf(stderr, "L%d: usage: push integer\n", line_num);
-        exit(EXIT_FAILURE);
-    }
-    for (i = 0; data.arg[i]; i++)
-    {
-        if (data.arg[i] == '+' || data.arg[i] == '-')
-        {
-            continue;
-        }
-        else if (!isdigit(data.arg[i]))
-        {
-            free(new_node);
-            free(data.line);
-            _free(stack);
-            fclose(data.fp);
-            fprintf(stderr, "L%d: usage: push integer\n", line_num);
-            exit(EXIT_FAILURE);
-        }
-    }
-    n = atoi(data.arg);
-    new_node->n = n;
-    if (*stack != NULL)
-    {
-        new_node->next = *stack;
-    }
-    else
-    {
-        new_node->next = NULL;
-    }
-    *stack = new_node;
+	if (new_node == NULL)
+	{
+		free(data.line);
+		_free(stack);
+		fclose(data.fp);
+		fprintf(stderr, "Error: Malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	if (data.arg == NULL)
+	{
+		free(new_node);
+		free(data.line);
+		_free(stack);
+		fclose(data.fp);
+		fprintf(stderr, "L%d: usage: push integer\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+	for (i = 0; data.arg[i]; i++)
+	{
+		if (data.arg[i] == '+' || data.arg[i] == '-')
+			continue;
+		else if (!isdigit(data.arg[i]))
+		{
+			free(new_node);
+			free(data.line);
+			_free(stack);
+			fclose(data.fp);
+			fprintf(stderr, "L%d: usage: push integer\n", line_num);
+			exit(EXIT_FAILURE);
+		}
+	}
+	n = atoi(data.arg);
+	new_node->n = n;
+	if (*stack != NULL)
+	{
+		new_node->next = *stack;
+	}
+	else
+	{
+		new_node->next = NULL;
+	}
+	*stack = new_node;
 }
 
 /**
@@ -63,14 +61,14 @@ void push(stack_t **stack, unsigned int line_num)
 */
 void pall(stack_t **stack, unsigned int line_num)
 {
-    stack_t *current = *stack;
-    (void)line_num;
+	stack_t *current = *stack;
+	(void)line_num;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
 }
 /**
  * pint - print value at the top of the stack
@@ -79,12 +77,12 @@ void pall(stack_t **stack, unsigned int line_num)
 */
 void pint(stack_t **stack, unsigned int line_num)
 {
-    if (*stack == NULL)
-    {
-        free(data.line);
-        fclose(data.fp);
-        fprintf(stderr, "L%d: can't pint, stack empty\n", line_num);
-        exit(EXIT_FAILURE);
-    }
-    printf("%d\n", (*stack)->n);
+	if (*stack == NULL)
+	{
+		free(data.line);
+		fclose(data.fp);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
 }
